3e0fb51905b90fd1dc452ad6244bee74c9a4188e
Stanislav Sinitsyn <stasov8@gmail.com>
2022-01-16 22:34:08 +0200

    HomeWork5

---
+++ src/main/java/Lesson5/Employee.java
@@ -0,0 +1,71 @@
+package Lesson5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public static void printEmployeeInfo(Employee employee){
+        System.out.println("| Full name is: " + employee.fullName +
+                "| Position is: " + employee.position + "| Email is: " + employee.email + "| Phone is: " + employee.phone + "| Salary is: " + employee.salary + "| Age is: " + employee.age + ".|");
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
